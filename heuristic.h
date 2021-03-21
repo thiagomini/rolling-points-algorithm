3ecbce0a43dcde86d8bb72f61e302aed5b9a57b4
Thiago Valentim <thiago_valentim1@hotmail.com>
2021-03-21 13:37:18 -0300

    fix: adição de documentação no arquivo de heuristicas

--- heuristic.h
+++ heuristic.h
@@ -8,6 +8,14 @@
 #include <cstdio>
 #include "data_structures/solution.h"
 
+/**
+ * Heurística iterativa aleatória, funciona com base na criação de várias soluções aleatórias e comparando
+ * os valores de suas FOs. O algoritmo para ao fim de um número de iterações definido.
+ *
+ * @param distance_matrix A matriz de distâncias entre os vértices do grafo
+ * @param number_of_vertices O número de vértices contidos no grafo
+ * @return Ponteiro para a melhor solução
+ */
 Solution * random_iterative_heuristic(int * distance_matrix, size_t number_of_vertices);
 
 int test_heuristic();
