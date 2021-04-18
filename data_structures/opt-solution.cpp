7f0cbfcc849bd2eff80c10d3727ba38af4da9512
Thiago Valentim <thiago_valentim1@hotmail.com>
2021-04-18 17:18:15 -0300

    docs: documentação da classe opt_solution

--- data_structures/opt-solution.cpp
+++ data_structures/opt-solution.cpp
@@ -27,16 +27,16 @@ void Opt_Solution::concatenate(const Opt_Solution& other_solution, const distanc
 }
 
 int Opt_Solution::concatenate_T(const Opt_Solution& other_solution, const distance_matrix distance_matrix) {
-    int new_vertex_distance = calculate_new_vertex_distance(other_solution, distance_matrix);
+    int new_vertex_distance = calculate_new_edge_distance(other_solution, distance_matrix);
     return T + new_vertex_distance + other_solution.T;
 }
 
 int Opt_Solution::concatenate_C(const Opt_Solution& other_solution, distance_matrix distance_matrix) {
-    int new_vertex_distance = calculate_new_vertex_distance(other_solution, distance_matrix);
+    int new_vertex_distance = calculate_new_edge_distance(other_solution, distance_matrix);
     return C + other_solution.W * (T + new_vertex_distance) + other_solution.C;
 }
 
-int Opt_Solution::calculate_new_vertex_distance(const Opt_Solution& other_solution, distance_matrix distance_matrix) {
+int Opt_Solution::calculate_new_edge_distance(const Opt_Solution& other_solution, distance_matrix distance_matrix) {
     int solution_1_last_vertex = vertices.back();
     int solution_2_first_vertex = other_solution.vertices.front();
     return distance_matrix.distances[solution_1_last_vertex * distance_matrix.size + solution_2_first_vertex];
--- data_structures/opt-solution.h
+++ data_structures/opt-solution.h
@@ -10,25 +10,81 @@
 #include "../configurations.h"
 #include "distance-matrix.h"
 
+
 class Opt_Solution {
+
 public:
+    // Indica o tamanho da solução, descontando o V0
     int W;
+
+    // Indica a soma da latência de cada vértice dessa solução
     int C;
+
+    // Indica a soma do custo das arestas dessa solução
     int T;
+
+    // A lista contendo os vértices da solução
     std::vector<int> vertices;
 
+    /**
+     *
+     * @param fo - Valor da função objetivo da solução
+     * @param size - O tamanho da solução, descontando-se o V0
+     * @param total_cost - A soma do custo de cada aresta da solução
+     * @param vertices_of_solution - A lista contendo os vértices da solução
+     */
     Opt_Solution(int fo, int size, int total_cost, std::vector<int> vertices_of_solution);
 
+    /**
+     * Concatena a solução atual com uma nova, unindo o caminho parcial de ambas
+     * @param other_solution
+     * @param distance_matrix
+     */
     void concatenate(const Opt_Solution& other_solution, distance_matrix distance_matrix);
 
+    /**
+     * Clona a solução atual
+     * @return Uma nova solução com as mesmas propriedades que a original
+     */
     Opt_Solution clone() const;
 
 private:
+    /**
+     * Concatena o valor de T(S) da solução atual com a de outra solução
+     * @param other_solution
+     * @param distance_matrix
+     * @return o novo valor de T(S)
+     */
     int concatenate_T(const Opt_Solution& other_solution, distance_matrix distance_matrix);
+
+    /**
+     * Concatena o valor de C(S) da solução atual com a de uma outra solução
+     * @param other_solution
+     * @param distance_matrix
+     * @return o novo valor de C(S)
+     */
     int concatenate_C(const Opt_Solution& other_solution, distance_matrix distance_matrix);
-    int calculate_new_vertex_distance(const Opt_Solution& other_solution, distance_matrix distance_matrix);
+
+    /**
+     * Calcula a distância da nova aresta inserida ao concatenar-se duas soluções
+     * @param other_solution
+     * @param distance_matrix
+     * @example
+     * solucao_1 = { 0, 1, 2 }<br>
+     * solucao_2 = { 3, 4, 5 }<br>
+     * solucao_1.concatenate(solucao_2); // Nova aresta criada: {2, 3}
+     * @return a distância da nova aresta criada
+     */
+    int calculate_new_edge_distance(const Opt_Solution& other_solution, distance_matrix distance_matrix);
 };
 
+/**
+ * Cria uma nova solução à partir da concatenação de duas soluções distintas
+ * @param solution_1
+ * @param solution_2
+ * @param distance_matrix
+ * @return
+ */
 Opt_Solution concatenate_solutions(const Opt_Solution& solution_1, const Opt_Solution& solution_2, distance_matrix distance_matrix);
 
 int test_opt_solution();
